72914c1574ce3f62b00d5d72bb986eeb6e5327f9
Vinicius Aquino <viniciussvl@hotmail.com>
2022-09-16 13:22:44 -0300

    add axios request post create game ads

--- src/components/CreateAdModal.tsx
+++ src/components/CreateAdModal.tsx
@@ -3,7 +3,8 @@ import { Input } from './Form/Input';
 import * as Dialog from '@radix-ui/react-dialog'
 import * as Checkbox from '@radix-ui/react-checkbox'
 import * as ToggleGroup from '@radix-ui/react-toggle-group';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
+import axios from 'axios';
 
 interface Game {
     id: string;
@@ -14,13 +15,45 @@ interface CreateAdModalProps {
     games: Game[]
 }
 
-
-
-
 export function CreateAdModal(props: CreateAdModalProps) {
     const validWeekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 
     const [weekDays, setWeekDays] = useState<string[]>(['1']);
+    const [useVoiceChannel, setUseVoiceChannel] = useState(false);
+
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+        const data = Object.fromEntries(formData)
+        console.log(data);
+        console.log(weekDays)
+        console.log(useVoiceChannel)
+
+
+        if(!data.name) {
+            return;
+        }
+
+        try {
+            await axios.post(`http://localhost:3333/games/${data.game}/ads`, {
+                name: data.name,
+                yearsPlaying: Number(data.yearsPlaying),
+                discord: data.discord,
+                weekDays: weekDays.map(Number),
+                hourStart: data.hourStart,
+                hourEnd: data.hourEnd,
+                useVoiceChannel: useVoiceChannel
+            })
+
+            alert('Anuncio criado com sucesso')
+
+        } catch (error) {
+            alert('Error')
+            console.log(error)
+        }
+
+    }
 
     return (
         <Dialog.Portal>
@@ -29,11 +62,12 @@ export function CreateAdModal(props: CreateAdModalProps) {
         <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-black/25">
           <Dialog.Title className="text-3xl font-black">Publique um anúncio</Dialog.Title>
 
-          <form className="mt-8 flex flex-col gap-4">
+          <form onSubmit={handleSubmit} className="mt-8 flex flex-col gap-4">
               <div className="flex flex-col gap-2">
                 <label className="font-semibold" htmlFor="game">Qual o game?</label>
                 <select 
                     id="game" 
+                    name="game"
                     defaultValue=""
                     className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500 appearance-none">
                         <option disabled value="">Selecione o game que deseja jogar</option>
@@ -45,19 +79,19 @@ export function CreateAdModal(props: CreateAdModalProps) {
               </div>
 
               <div className="flex flex-col gap-2">
-                <label htmlFor="username">Seu nome (ou nickname)</label>
-                <Input id="username" type="text" placeholder="Como te chamam dentro do game?" />
+                <label htmlFor="name">Seu nome (ou nickname)</label>
+                <Input id="name" name="name" type="text" placeholder="Como te chamam dentro do game?" />
               </div>
 
               <div className="grid grid-cols-2 gap-6">
                 <div className="flex flex-col gap-2">
                   <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
-                  <Input id="yearsPlaying" type="text" placeholder="Tudo bem ser zero" />
+                  <Input id="yearsPlaying" name="yearsPlaying" type="text" placeholder="Tudo bem ser zero" />
                 </div>
 
                 <div className="flex flex-col gap-2">
                   <label htmlFor="discord">Qual seu discord?</label>
-                  <Input id="discord" type="text" placeholder="Usuario#0000" />
+                  <Input id="discord" name="discord" type="text" placeholder="Usuario#0000" />
                 </div>
               </div>
 
@@ -73,7 +107,7 @@ export function CreateAdModal(props: CreateAdModalProps) {
                         >
 
                         { validWeekDays.map((day, index) =>
-                            <ToggleGroup.Item value={index.toString()} 
+                            <ToggleGroup.Item key={index} value={index.toString()} 
                                 className={`w-8 h-8 rounded ${weekDays.includes(`${index}`) ? 'bg-violet-500' : 'bg-zinc-900' } `}>
                                 {day}
                             </ToggleGroup.Item>
@@ -87,14 +121,24 @@ export function CreateAdModal(props: CreateAdModalProps) {
                   <label htmlFor="weekDays">Qual horário do dia?</label>
 
                   <div className="grid grid-cols-2 gap-1">
-                    <Input id="hourStart" type="time" placeholder="De" />
-                    <Input id="hourEnd" type="time" placeholder="Até" />
+                    <Input id="hourStart" name="hourStart" type="time" placeholder="De" />
+                    <Input id="hourEnd" name="hourEnd" type="time" placeholder="Até" />
                   </div>
                 </div>
               </div>
 
               <label className="mt-2 flex items-center gap-2 text-sm">
-                <Checkbox.Root className="w-6 h-6 p-1 rounded bg-zinc-900">
+                <Checkbox.Root 
+                    className="w-6 h-6 p-1 rounded bg-zinc-900" 
+                    checked={useVoiceChannel}
+                    onCheckedChange={(checked) => {
+                        if(checked === true) {
+                            setUseVoiceChannel(true)
+                        } else {
+                            setUseVoiceChannel(false)
+                        }
+                    }}>
+
                     <Checkbox.Indicator>
                         <Check className="w-4 h-4 text-emerald-400" />
                     </Checkbox.Indicator>
